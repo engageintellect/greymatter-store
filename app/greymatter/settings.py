b18a8e298b809b6e20872fbdfad9edbc05152206
engageintellect <jc9361@gmail.com>
2023-09-18 08:54:28 +0000

    init prod

--- app/greymatter/settings.py
+++ app/greymatter/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-o0br^cv6#og6gyby0e5@i19fb4#rxn2bp00qf8es#8@(+vim^i
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['172.233.128.129']
 
 LOGOUT_REDIRECT_URL = '/'
 LOGIN_REDIRECT_URL = '/'
@@ -132,6 +132,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = 'static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media/'
---
+++ venv/lib/python3.11/site-packages/stripe/api_resources/payment_method_domain.py
@@ -0,0 +1,128 @@
+# -*- coding: utf-8 -*-
+# File generated from our OpenAPI spec
+from __future__ import absolute_import, division, print_function
+
+from stripe import util
+from stripe.api_resources.abstract import (
+    CreateableAPIResource,
+    ListableAPIResource,
+    UpdateableAPIResource,
+)
+from stripe.api_resources.list_object import ListObject
+from stripe.stripe_object import StripeObject
+from typing import cast
+from typing_extensions import Literal
+from urllib.parse import quote_plus
+
+
+class PaymentMethodDomain(
+    CreateableAPIResource["PaymentMethodDomain"],
+    ListableAPIResource["PaymentMethodDomain"],
+    UpdateableAPIResource["PaymentMethodDomain"],
+):
+    """
+    A payment method domain represents a web domain that you have registered with Stripe.
+    Stripe Elements use registered payment method domains to control where certain payment methods are shown.
+
+    Related guides: [Payment method domains](https://stripe.com/docs/payments/payment-methods/pmd-registration).
+    """
+
+    OBJECT_NAME = "payment_method_domain"
+    apple_pay: StripeObject
+    created: int
+    domain_name: str
+    enabled: bool
+    google_pay: StripeObject
+    id: str
+    link: StripeObject
+    livemode: bool
+    object: Literal["payment_method_domain"]
+    paypal: StripeObject
+
+    @classmethod
+    def create(
+        cls,
+        api_key=None,
+        idempotency_key=None,
+        stripe_version=None,
+        stripe_account=None,
+        **params
+    ) -> "PaymentMethodDomain":
+        return cast(
+            "PaymentMethodDomain",
+            cls._static_request(
+                "post",
+                cls.class_url(),
+                api_key,
+                idempotency_key,
+                stripe_version,
+                stripe_account,
+                params,
+            ),
+        )
+
+    @classmethod
+    def list(
+        cls, api_key=None, stripe_version=None, stripe_account=None, **params
+    ) -> ListObject["PaymentMethodDomain"]:
+        result = cls._static_request(
+            "get",
+            cls.class_url(),
+            api_key=api_key,
+            stripe_version=stripe_version,
+            stripe_account=stripe_account,
+            params=params,
+        )
+        if not isinstance(result, ListObject):
+
+            raise TypeError(
+                "Expected list object from API, got %s"
+                % (type(result).__name__)
+            )
+
+        return result
+
+    @classmethod
+    def modify(cls, id, **params) -> "PaymentMethodDomain":
+        url = "%s/%s" % (cls.class_url(), quote_plus(id))
+        return cast(
+            "PaymentMethodDomain",
+            cls._static_request("post", url, params=params),
+        )
+
+    @classmethod
+    def retrieve(cls, id, api_key=None, **params) -> "PaymentMethodDomain":
+        instance = cls(id, api_key, **params)
+        instance.refresh()
+        return instance
+
+    @classmethod
+    def _cls_validate(
+        cls,
+        payment_method_domain,
+        api_key=None,
+        stripe_version=None,
+        stripe_account=None,
+        **params
+    ):
+        return cls._static_request(
+            "post",
+            "/v1/payment_method_domains/{payment_method_domain}/validate".format(
+                payment_method_domain=util.sanitize_id(payment_method_domain)
+            ),
+            api_key=api_key,
+            stripe_version=stripe_version,
+            stripe_account=stripe_account,
+            params=params,
+        )
+
+    @util.class_method_variant("_cls_validate")
+    def validate(self, idempotency_key=None, **params):
+        return self._request(
+            "post",
+            "/v1/payment_method_domains/{payment_method_domain}/validate".format(
+                payment_method_domain=util.sanitize_id(self.get("id"))
+            ),
+            idempotency_key=idempotency_key,
+            params=params,
+        )
