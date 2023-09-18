b1889d314c1d9e1eb0b197cc85ce0688c421a9c2
engageintellect <jc9361@gmail.com>
2023-09-18 11:19:53 +0000

    working deployment

--- app/greymatter/wsgi.py
+++ app/greymatter/wsgi.py
@@ -1,16 +1,11 @@
-"""
-WSGI config for greymatter project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
-"""
-
+# wsgi.py
 import os
-
+import sys
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'greymatter.settings')
+# Add your project's root directory to the Python path
+sys.path.append('/home/greymatter/greymatter-store/app')
 
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'greymatter.settings')
 application = get_wsgi_application()
+
