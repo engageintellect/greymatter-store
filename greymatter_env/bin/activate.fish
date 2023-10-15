7eb2f4c443e7edbf27bda3f30ae4362c85925222
engageintellect <jc9361@gmail.com>
2023-10-14 19:36:27 -0700

    init

--- greymatter_env/bin/activate.fish
+++ greymatter_env/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/r3dux/greymatter/greymatter_env"
+set -gx VIRTUAL_ENV "/home/r3dux/Repositories/greymatter/greymatter_env"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
